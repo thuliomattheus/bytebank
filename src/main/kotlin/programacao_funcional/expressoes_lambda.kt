65ed3147901c89340bc5ec3d1cfe495292a1e417
Thúlio Sousa <thuliomattheus@hotmail.com>
2022-03-15 15:55:19 -0300

    melhorando expressoes lambda e funcoes anonimas

--- src/main/kotlin/programacao_funcional/expressoes_lambda.kt
+++ src/main/kotlin/programacao_funcional/expressoes_lambda.kt
@@ -1,21 +1,29 @@
 package programacao_funcional
 
+import kotlin.math.pow
+
 fun main() {
-    val funcaoInutil: () -> Unit = {
-        println("Não retorna nada")
-    }
+    val funcaoInutil: () -> Unit = { println("Não retorna nada") }
     println(funcaoInutil())
 
-    val funcaoInutilSemDeclararOTipo = {
-        println("Aqui também não retorna nada")
-    }
+    val funcaoInutilSemDeclararOTipo = { println("Aqui também não retorna nada") }
     println(funcaoInutilSemDeclararOTipo())
 
     val mil: () -> Int = {1000}
     println(mil())
 
-    val dobro: (param: Int) -> Int = {
-        2 * it
-    }
+    val dobro: (Int) -> Int = { 2 * it }
     println("O dobro de 5 é ${dobro(5)}")
+
+    val metade: (numero: Int) -> Int = { n -> n/2 }
+    println("A metade de 100 é ${metade(100)}")
+
+    val soma: (Int, Int) -> Int = { a, b -> a + b }
+    println("A soma de 23 e 9 é ${soma(23, 9)}")
+
+    val multiplicacao = { x: Int, y: Int -> x * y }
+    println("A multiplicação de 15 e 4 é ${multiplicacao(15, 4)}")
+
+    val quadradoDoSegundo = { _: Double, t: Double -> t.pow(2).toInt() }
+    println("O quadrado de 100 é ${quadradoDoSegundo(78.0, 100.0)}")
 }
--- src/main/kotlin/programacao_funcional/usando_funcoes_anonimas.kt
+++ src/main/kotlin/programacao_funcional/usando_funcoes_anonimas.kt
@@ -1,23 +1,15 @@
 package programacao_funcional
 
 fun main() {
-    val funcaoInutil: () -> Unit = fun () {
-        println("Não retorna nada")
-    }
+    val funcaoInutil: () -> Unit = fun () { println("Não retorna nada") }
     println(funcaoInutil())
 
-    val funcaoInutilSemDeclararOTipo = fun () {
-        println("Aqui também não retorna nada")
-    }
+    val funcaoInutilSemDeclararOTipo = fun () { println("Aqui também não retorna nada") }
     println(funcaoInutilSemDeclararOTipo())
 
-    val mil: () -> Int = fun () : Int {
-        return 1000
-    }
+    val mil: () -> Int = fun () : Int { return 1000 }
     println(mil())
 
-    val dobro: (param: Int) -> Int = fun (numero) : Int {
-        return 2 * numero
-    }
+    val dobro: (param: Int) -> Int = fun (numero) : Int { return 2 * numero }
     println("O dobro de 5 é ${dobro(5)}")
 }
