3c0d08f405e597b331343cf8290502fcc9f3e6b9
Thúlio Sousa <thuliomattheus@hotmail.com>
2022-03-17 12:40:24 -0300

    usando HOF com receiver

--- src/main/kotlin/programacao_funcional/high_order_functions.kt
+++ src/main/kotlin/programacao_funcional/high_order_functions.kt
@@ -13,10 +13,29 @@ fun main() {
     soma(3, 7) {
         println("O resultado $it " + if(it % 2 ==0) "é par" else "é ímpar")
     }
+
+    // Exemplo 1 com receiver
+    somaComReceiver(12, 8) {
+        println("A soma de a e b é: $this")
+    }
+
+    // Exemplo 2 com receiver
+    somaComReceiver(5, 10, resultado = (::println))
+
+    somaComReceiver(3, 7) {
+        println("O resultado $this " + if(this % 2 ==0) "é par" else "é ímpar")
+    }
 }
 
 private fun soma(a: Int, b:Int, resultado: (Int) -> Unit) {
     println("A função soma foi iniciada")
     resultado(a+b)
+    println("A função soma foi finalizada")
+}
+
+private fun somaComReceiver(a: Int, b: Int, resultado: Int.() -> Unit) {
+    println("A função soma foi iniciada")
+    var total = a + b
+    total.resultado()
     println("A função soma foi finalizada")
 }
